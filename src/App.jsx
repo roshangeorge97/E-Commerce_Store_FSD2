ef362560afa3bd53c7d6492eeebe0e0cf20cee0c
Roshan George <roshangeorge2003@gmail.com>
2024-10-08 19:43:46 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
         <Route path='/place-order' element={<PlaceOrder/>} />
         <Route path='/orders' element={<Orders/>} />
       </Routes>
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   )
 }
